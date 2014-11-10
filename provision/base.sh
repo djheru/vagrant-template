97899aab9e1954b77f06d4076f5833d33b219b32
djheru <djheru@gmail.com>
2014-11-10 09:04:59 -0500

    added postgres to the base provision script

--- provision/base.sh
+++ provision/base.sh
@@ -23,3 +23,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 > /de
 sudo sh -c 'echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" | sudo tee /etc/apt/sources.list.d/mongodb.list' > /dev/null
 sudo apt-get update
 sudo apt-get install -y mongodb-org
+
+echo "--- Installing Postgres ---"
+sudo apt-get install postgresql-9.3
