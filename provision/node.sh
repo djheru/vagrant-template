b9610aa3abe17f3d18f57f397e6184eea9df0df0
Philip Damra <pdamra@philipdamra.com>
2015-08-08 14:05:47 -0400

    Remove some global npm packages

--- provision/node.sh
+++ provision/node.sh
@@ -5,22 +5,16 @@ sudo apt-get update
 sudo su vagrant -c "wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh"
 source /home/vagrant/.nvm/nvm.sh
 source /home/vagrant/.bashrc
-nvm install 0.11
-nvm alias default 0.11
+nvm install 0.12
+nvm alias default 0.12
 nvm use default
 
 echo "-- Installing some useful global packages --"
 npm install -g express-generator
-npm install -g nodemon
 npm install -g gulp
 npm install -g karma
-npm install -g yo
 npm install -g bower
 npm install -g jasmine
-npm install -g generator-karma
-npm install -g generator-angular
-npm install -g generator-angular-fullstack
 npm install -g phantomjs
 npm install -g pm2
-npm install -g strongloop
 
